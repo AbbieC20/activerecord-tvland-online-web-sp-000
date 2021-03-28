035fed65bf61663a6d0dc0d90414464f6f304f11
remote-torrent-6747 <abbyc@sky.com>
2021-03-28 15:49:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
+    self.shows.map {|actor| actor.name}
   end
-  
+
 end
+
+# self.characters.map {|character| "#{character.name} - #{character.show.name}"}
