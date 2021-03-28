a943025322f4f89c518e49283cf3ea12171a729a
remote-torrent-6747 <abbyc@sky.com>
2021-03-28 16:10:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,7 @@ class Show < ActiveRecord::Base
   end
 
   def build_network(call_letters:)
-    self.show = Network.new(call_letters: call_letters)
+    self.network = Network.new(call_letters: call_letters)
   end
 
 end
