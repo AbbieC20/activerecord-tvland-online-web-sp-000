440f6d8500924e095ee565d2f5a59bbbd335b18a
remote-torrent-6747 <abbyc@sky.com>
2021-03-28 16:06:35 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,7 +6,22 @@ class Character < ActiveRecord::Base
     "#{self.name} always says: #{self.catchphrase}"
   end
 
-  def build_show
-  end 
+  def build_show(name:)
+    self.show = Show.new(name: name)
+  end
 
+end
+
+it "can build its associated show" do
+  jules_cobb = Character.new(name: "Jules Cobb")
+  jules_cobb.build_show(name: "Cougar Town")
+  expect(jules_cobb.show.name).to eq("Cougar Town")
+end
+
+it "can chain-build associations to which it belongs" do
+  malcolm = Character.new(name: "Malcolm Reynolds")
+  malcolm.build_show(name: "Firefly").build_network(:call_letters => "Fox")
+  show = malcolm.show
+  expect(show.name).to eq("Firefly")
+  expect(show.network.call_letters).to eq("Fox")
 end
