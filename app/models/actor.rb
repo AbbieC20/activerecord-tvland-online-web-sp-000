776b0cd3115a38e9e536101040dd1e418d3bf7f7
remote-torrent-6747 <abbyc@sky.com>
2021-03-28 15:23:33 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return "#{actors.first_name} #{actors.last_name}"
+  end
+
 end
+
+# has associated characters in an array
+# can build its associated characters
+# can build its associated shows through its characters
+# can list its full name
+# can list all of its shows and characters
